e53a138a72495994cec5d3e89bc909b78587feee
RubenNhez <ruben.nhez@gmail.com>
2023-11-23 18:11:25 +0000

    add food to each course code, tab icon and menu

---
+++ routes/main.js
@@ -0,0 +1,384 @@
+module.exports = function(app, shopData) {
+    // Redirect Login
+    const redirectLogin = (req,res,next) => {
+        if (!req.session.userId) {
+            res.redirect('./login')
+        } else {next ();}
+    }
+
+const { check, validationResult } = require ('express-validator');
+
+    // Handle our routes
+    app.get('/',function(req,res){
+        res.render('index.ejs', shopData)
+    });
+    app.get('/about',function(req,res){
+        res.render('about.ejs', shopData);
+    });
+    app.get('/search',function(req,res){
+        res.render("search.ejs", shopData);
+    });
+    app.get('/search-result', function (req, res) {
+        //searching in the database
+        //res.send("You searched for: " + req.query.keyword);
+        let sqlquery= "SELECT * FROM books WHERE name LIKE '%" + req.query.keyword + "%'";
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+            let searchData = Object.assign({}, shopData, {searchBooks: result});
+            console.log(searchData)
+            res.render("search-result.ejs", searchData);
+        });
+    });
+    app.get('/register', function (req,res) {
+        res.render('register.ejs', shopData);                                                                     
+    });                                                                                                 
+    app.post('/registered', [check('email').isEmail()], function (req,res) {
+        // saving data in database
+       // res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);
+        const errors = validationResult(req);
+        const bcrypt = require('bcrypt');
+        const saltRounds = 10;
+        const plainPassword = req.body.password;
+        if(!errors.isEmpty()) {
+            res.redirect('./register');
+        }
+        else {
+            bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+                if (err) {
+                  return console.error(err.message)
+                }
+                let sqlquery = "INSERT INTO users (firstname,lastname,email,username,hashedpassword) VALUES (?,?,?,?,?)";
+                let newrecord = [req.sanitize(req.body.first),req.body.last,req.body.email,req.body.username,hashedPassword];
+      
+                db.query(sqlquery,newrecord, (err, result) => {
+                  if(err) {
+                      return console.error(err.message);
+                  }
+                  else {
+                      result = 'Hello '+ req.sanitize(req.body.first) + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email;
+                      result += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword;
+                      res.send(result);
+      
+      
+                  }
+                })
+      
+      
+              })
+        } 
+        
+    });
+
+    //Login Page
+    app.get('/login', function (req,res) {
+        res.render('login.ejs', shopData);                                                                     
+    }); 
+
+    app.post('/loggedin', function (req,res) {
+
+        const bcrypt = require('bcrypt');
+
+        let sqlquery = "SELECT hashedpassword FROM users WHERE username =?";
+        
+        let userdata = req.body.username;//SELECT hashedpassword FROM userdetails WHERE username = "abc"
+
+        db.query(sqlquery, userdata, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                //hashedPassword = result[0].hashedpassword;
+                //console.log(hashedpassword);
+                console.log(req.body.password);
+                console.log(result);
+                bcrypt.compare(req.body.password, result[0].hashedpassword, function(err, result) {
+                    if (err) {
+                      // TODO: Handle error
+                      res.send( 'There is an error');
+                    }
+                    else if (result == true) {
+
+                      req.session.userId = req.body.username;
+                      res.send('That is the correct password');
+                      //<a href=' + '/' + '>HOME</a>'
+                    
+                    }
+                    else {
+                      // TODO: Send message
+                      res.send('That is the wrong password');
+                    }
+                  });
+            }
+        })
+    })
+
+    app.get('/logout', redirectLogin, (req,res) => {
+        req.session.destroy(err => {
+            if (err) {
+                return res.redirect('./')
+            }
+            res.send('you are now logged out. <a href='+'./'+'>Home</a>');
+        })
+    })   
+
+        // hashedPassword = sqlquery
+
+        // bcrypt.compare(req.body.password, hashedPassword, function(err, result) {
+        //     if (err) {
+        //       // TODO: Handle error
+        //       res.send( 'There is an error')
+        //     }
+        //     else if (result == true) {
+        //       // TODO: Send message
+        //       res.send('That is the correct password')
+        //     }
+        //     else {
+        //       // TODO: Send message
+        //       res.send('That is the wrong password')
+        //     }
+        //   });
+      
+    
+
+    
+    app.post('/delete', function (req,res) {
+        let sqlquery = 'DELETE FROM users WHERE username = "' + req.body.username +'"'
+        let DeleteUsername = req.body.username
+
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.send('there was an error')
+            }
+            else {
+                if(result.affectedRows == 0) {
+                    res.send("No user with that username")
+                }
+                else{
+                    res.send("User " + DeleteUsername + "delete successfully!")
+                }
+            }
+    })
+})
+
+    
+    
+    //Delete Page
+    app.get('/deleteuser', function (req,res) {
+        res.render('deleteuser.ejs', shopData);                                                                     
+    }); 
+
+    //List of Users
+    app.get('/listusers', function(req, res) {
+        let sqlquery = "SELECT firstname, lastname, email, username FROM users"; // query database to get all users
+        // exxecute sql query
+        db.query(sqlquery, (err, result) => {
+            if(err) {
+                res.redirect('./');
+            }
+            let newData = Object.assign({}, shopData, {availableusers: result});
+            console.log(newData)
+            res.render("listusers.ejs", newData)
+
+        })
+    })
+
+
+    // Weather Forecast
+    app.get('/weather', function(req,res) {
+        const request = require('request');
+
+        let apiKey = '4066b0791a9932795eb1fe9004cb04f7';
+        let city = 'london';
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+        request(url, function (err,response, body) {
+            if(err){
+                console.log('error:', error);
+            } else {
+                // res.send(body);
+                var weather = JSON.parse(body)
+                if (weather!==undefined && weather.main!== undefined) {
+                var wmsg = 'It is ' + weather.main.temp + ' degrees in ' + weather.name +  ' The maximum temp is ' + weather.main.temp_max + ' And the minimum temp is ' + weather.main.temp_min + '! <br> The humidity now is: ' + weather.main.humidity +  '! <br> The pressure now is: ' + weather.main.pressure;
+                res.send(wmsg);
+                }
+                else {
+                    res.send("No data found");
+                }
+            }
+        });
+    })
+// DONT FORGET THE redirectLogin
+    //List of Starters, Mains And Desserts
+    // app.get('/list', function(req, res) {
+        
+        
+    //     let alldesserts = "SELECT * FROM desserts"; //query database to get all the books
+    //     // execute sql query
+    //     db.query(alldesserts, (err, result) => {
+    //         if (err) {
+    //             return res.redirect('/');
+    //         }
+
+    //         let dessertfood = Object.assign({}, shopData, {availableDesserts: result});
+    //         console.log(dessertfood)
+    //         res.render("list.ejs", dessertfood)
+    //     });
+    // });
+
+
+    app.get('/listmain', function(req, res) {
+
+        let sqlquery = "SELECT * FROM starters"; //query database to get all the books
+        // execute sql query
+        db.query(sqlquery, (err, resultStarters) => {
+            if (err) {
+                return res.redirect('/');
+            }
+
+            let allmains = "SELECT * FROM mains"; //query database to get all the books
+            // execute sql query
+            db.query(allmains, (err, resultMains) => {
+                if (err) {
+                    return res.redirect('/');
+                }
+    
+                let food = Object.assign({}, shopData, {availableStarters: resultStarters, availableMains: resultMains});
+                res.render("listmain.ejs", food)
+            });
+        });
+    });
+
+    app.get('/listmain', function(req, res) {
+        let allmains = "SELECT * FROM mains"; //query database to get all the books
+        // execute sql query
+        db.query(allmains, (err, result) => {
+            if (err) {
+                return res.redirect('/');
+            }
+
+            let mainsfood = Object.assign({}, shopData, {availableStarters: result});
+            console.log(mainsfood)
+            res.render("listmain.ejs", mainsfood)
+        });
+
+    });
+
+
+    app.get('/addstarter', function (req,res) {
+        //Adding starter
+        res.render('addstarter.ejs', shopData);
+    })
+
+    app.post('/starteradded', function (req,res) {
+        // saving data in database
+        if(req.body.price >= 4.99) {
+        let sqlquery = "INSERT INTO starters (starter_name, starter_price) VALUES (?,?)";
+        // execute sql query
+        let newrecord = [req.body.name, req.body.price];
+        db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        else
+        res.send(' This starter is added to database, name: '+ req.body.name + ' price '+ req.body.price);
+        });
+        }
+        else {
+            res.send('The minimum price for a starter is £4.99')
+        }
+    });
+
+
+    app.get('/addstarter', function (req,res) {
+        //Adding starter
+        res.render('addstarter.ejs', shopData);
+    })
+
+    app.post('/starteradded', function (req,res) {
+        // saving data in database
+        if(req.body.price >= 5.99) {
+        let sqlquery = "INSERT INTO starters (starter_name, starter_price) VALUES (?,?)";
+        // execute sql query
+        let newrecord = [req.body.name, req.body.price];
+        db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        else
+        res.send(' This starter is added to database, name: '+ req.body.name + ' price '+ req.body.price);
+        });
+        }
+        else {
+            res.send('The minimum price for a starter is £5.99')
+        }
+    });
+
+
+    app.get('/addmain', function (req,res) {
+        //Adding Main
+        res.render('addmain.ejs', shopData);
+    })
+
+    app.post('/mainadded', function (req,res) {
+        // saving data in database
+        if(req.body.price >= 10.99) {
+        let sqlquery = "INSERT INTO mains (main_name, main_price) VALUES (?,?)";
+        // execute sql query
+        let newrecord = [req.body.name, req.body.price];
+        db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        else
+        res.send(' This main is added to database, name: '+ req.body.name + ' price '+ req.body.price);
+        });
+        }
+        else {
+            res.send('The minimum price for a starter is £10.99')
+        }
+    });
+
+    app.get('/adddessert', function (req,res) {
+        //Adding starter
+        res.render('adddessert.ejs', shopData);
+    })
+
+    app.post('/dessertadded', function (req,res) {
+        // saving data in database
+        if(req.body.price >= 4.99) {
+        let sqlquery = "INSERT INTO desserts (dessert_name, dessert_price) VALUES (?,?)";
+        // execute sql query
+        let newrecord = [req.body.name, req.body.price];
+        db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        else
+        res.send(' This dessert is added to database, name: '+ req.body.name + ' price '+ req.body.price);
+        });
+        }
+        else {
+            res.send('The minimum price for a starter is £4.99')
+        }
+    });
+    //Bargain books
+    app.get('/bargainbooks', function(req, res) {
+        let sqlquery = "SELECT * FROM books"; //query database to get all the books
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                res.redirect('./');
+            }
+            let newData = Object.assign({}, shopData, {availableBooks: result});
+            console.log(newData)
+            res.render("bargainbooks.ejs", newData)
+        });
+    });
+}
+
+
+// 1 -  sql query - Select hashed password from userdetails where username = ? 
+// 2 - bcrypt compare the password to the userdetails hashed password
+//req.session.userId = req.body.username
